594d1f95faaf2be11ecb95369b58c9ce0255d34d
Martin Esteban Gutierrez <martin.gutierrez@inchcape.com>
2021-09-22 12:00:52 -0500

    Update polybar theme, tweak i3 gaps to accomadate for it.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,21 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+    while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar bot &
+# launch the foreground bars first to have them placed on top, it's not clear why this is so
+polybar wall-tile-right 2>&1 | tee -a /tmp/polybar.log & disown
+sleep 0.4s
+polybar wall-tile-right-background 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar launched..."
+polybar wall-tile-left 2>&1 | tee -a /tmp/polybar.log & disown
+sleep 0.4s
+polybar wall-tile-left-background 2>&1 | tee -a /tmp/polybar.log & disown
+
+polybar wall-tile-center-background 2>&1 | tee -a /tmp/polybar.log & disown
+
+polybar misc-bar 2>&1 | tee -a /tmp/polybar.log & disown
+
+#sleep 0.2s
+#polybar dummy
